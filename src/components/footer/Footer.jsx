6079b72c3c2bb65f1583f42e2389a8f8ae968d58
Gevor Hovhannisyan <gevorg20060204@gmail.com>
2024-05-28 15:05:14 +0400

    service page responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,86 +1,66 @@
 import Container from '../common/container/Container'
+import footerLogo from "../../assets/footer/img.png"
 import "./Footer.scss"
 import { CONFIG } from '../../config'
-import { useLocation } from 'react-router';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 
 const Footer = () => {
-    const [footerData, setFooterData] = useState([])
-    const location = useLocation();
 
-    useEffect(() => {
-      async function getData() {
-          try {
-              const footerData = await axios.get("https://globalitacademy.am/GIAcademyApi/footer/");
-              setFooterData(footerData.data);
-          } catch (error) {
-            
-              console.log("Error")
-          }
-      }
-      getData();
-  }, []);
-
-
-  if (location.pathname === '/auth') {
-
-    // location = useLocation().pathname;
+    const location = useLocation().pathname;
     
   if (location === '/auth') {
     return null;
   }
 
 
-
   return (
-    <footer style={{marginTop: location==="/service-info"?"100px":"0px"}}>
+    <footer style={{marginTop: location==="/service"? "20px": location==="/service-info" ? "20px": "0px"}}>
         <Container>
-            {footerData.map(({id, logo, text_hy, text_ru, text_en, fc, tw, ins, linked, address_hy, address_ru, addrees_en, phone, mail, back_text_hy, back_text_ru, back_text_en}) => {
-                return(
-                    <div className="footer-row" key={id}>
-                        <div className="footer-top">
-                            <div className="footer-top-about">
-                                <img src={logo} alt="" />
-                                <p>{text_hy}</p>
-                            </div>
-                            <div className="footer-top-firstList" >
-                                {CONFIG.footerFirstListConfig.map(({id,title,text}) => {
-                                    return(
-                                        <div className="footer-top-firstList-item" key={id}>
-                                            <h4>{title}</h4>
-                                            <p>{text}</p>
-                                        </div> 
-                                    )
-                                })}
-                            </div>
-                            <div className="footer-top-secondList">
-                                {CONFIG.footerSecondListConfig.map(({id, text}) => {
-                                    return(
-                                        <div className="footer-top-secondList-item" key={id}>
-                                            <p>{text}</p>
-                                        </div> 
-                                    )
-                                })}
-                            </div>
-                            <div className="footer-top-thirdList">
-                                <p>Հասցե՝   {address_hy}</p>
-                                <p>Հեռախոսահամար՝  {phone}</p>
-                                <p>Էլ․ հասցե՝  {mail}</p>
-                            </div>
-                        </div>
-                        <div className="footer-bottom">
-                            <p>{back_text_hy}</p>
-                        </div>
-                    </div>
-                )
-            })}
-            
+            <div className="footer-top">
+                <div className="footer-top-about">
+                    <img src={footerLogo} alt="" />
+                    <p>ԳԼՈԲԱԼ ԻՏ» ՍՊԸ-ն հիմնադրվել է 2017թ.-ին: Մենք առաջարկում ենք ծրագրային տարատեսակ լուծումներ` վեբ կայքերի պատրաստումից մինչև մոբայլ հավելվածներ, խաղեր,արհեստական բանականություն, ավտոմատացման համակարգեր և այլն: Իրականացնում ենք նաև աշխատաշուկայի պահանջներին համապատասխան ՏՏ ոլորտի դասընթացներ:</p>
+                </div>
+                <div className="footer-top-firstList" >
+                    {CONFIG.footerFirstListConfig.map(({id,title,text}) => {
+                        return(
+                            <div className="footer-top-firstList-item" key={id}>
+                                <h4>{title}</h4>
+                                <p>{text}</p>
+                            </div> 
+                        )
+                    })}
+                </div>
+                <div className="footer-top-secondList">
+                    {CONFIG.footerSecondListConfig.map(({id, text}) => {
+                        return(
+                            <div className="footer-top-secondList-item" key={id}>
+                                <p>{text}</p>
+                            </div> 
+                        )
+                    })}
+                </div>
+                <div className="footer-top-thirdList">
+                    {CONFIG.footerThirdListConfig.map(({id, text1, text2, text3}) => {
+                        return(
+                            <div className="footer-top-thirdList-item" key={id}>
+                                <p>Հասցե՝   {text1}</p>
+                                <p>Հեռախոսահամար՝ {text2}</p>
+                                <p>Էլ․ հասցե՝{text3}</p>
+                            </div> 
+                        )
+                    })}
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>Copyright ©2023 Բոլոր հեղինակային իրավունքները պաշտպանված են:<br /> 
+                Կայքը պատրաստված է "ԳԼՈԲԱԼ ԻՏ" (ՍՊԸ)-ի կողմից, որը հանդիսանում է ընկերության Պաշտոնական կայքէջը: Կայքից նույթ տեղադրելիս հղումը globalitc.am-ին պարտադիր է:</p>
+                <p className='responsive-text'>Copyright ©2023 Կայքից նույթ տեղադրելիս հղումը globalitc.am-ին պարտադիր է:</p>
+            </div>
         </Container>
     </footer>
   )
-}
 }
 
 export default Footer
